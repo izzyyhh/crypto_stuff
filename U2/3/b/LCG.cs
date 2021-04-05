409b0a1b38ec9db6d7bfb5a03d0886c640a2a366
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-04-05 02:43:35 +0200

    add more files

---
+++ U2/3/b/LCG.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class LCG {
+    public int a, c, m, x0;
+
+    public LCG(int a, int c, int m, int x0) {
+        this.a = a;
+        this.c = c;
+        this.m = m;
+        this.x0 = x0;
+    }
+
+    public int[] RandomSeries(int length) {
+        int[] result = new int[length];
+        result[0] = x0;
+
+        for(int count = 1; count < length; count++) {
+            result[count] = (a * result[count - 1] + c) % m; 
+        }
+
+        return result;
+    }
+}
---
+++ U2/3/b/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LCG lcg = new LCG(24, 42, 529, 42);
+            LCG lcg2 = new LCG(24, 42, 524, 42);
+
+            int[] series = lcg.RandomSeries(1000);
+            int[] series2 = lcg2.RandomSeries(1000);
+
+            Console.WriteLine("Series with modulo 529");
+
+            int counter = 0;
+
+            for(int i = 0; i < 1000; i++) {
+                if(series[i] == 42){
+                  Console.WriteLine("repeats"); 
+                  counter++;
+                }
+                Console.WriteLine(series2[i]);  
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Second series with modulo 524");
+
+            int counter2 = 0;
+
+            for(int i = 0; i < 1000; i++) {
+                if(series2[i] == 42){
+                  Console.WriteLine("repeats");
+                  counter2++;
+                }
+                Console.WriteLine(series2[i]);  
+            }
+
+            Console.WriteLine("Series 2 repeats more often: " + (counter2 > counter));
+
+        }
+    }
+}
