2f5d4ea7b144c742a18eaaa8701bea4e1e38007a
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-06-05 15:53:07 +0200

    add first num, assignment 6

---
+++ U6/1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class RSAOAEP
+{
+
+    static void Main()
+    {
+        try
+        {
+            //Create a UnicodeEncoder to convert between byte array and string.
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
+
+            byte[] encryptedData;
+            byte[] decryptedData;
+            byte[] encryptedData2;
+            byte[] decryptedData2;
+
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSAParameters publicKey = RSA.ExportParameters(false);
+                RSAParameters privateKey = RSA.ExportParameters(true);
+                bool useOAEP = true;
+
+                // 1a)
+                // Verschlüsseln Sie die gleiche Nachricht Ihrer Wahl bei gleichem Schlüssel zweimal mit RSA OAEP.
+
+                encryptedData = RSAEncrypt(dataToEncrypt, publicKey, useOAEP);
+                encryptedData2 = RSAEncrypt(dataToEncrypt, publicKey, useOAEP);
+
+                Console.WriteLine("Verschlüsselung mit gleichem Key.");
+                Console.WriteLine();
+                Console.WriteLine("1: {0}", Convert.ToBase64String(encryptedData));
+                Console.WriteLine();
+                Console.WriteLine("2: {0}", Convert.ToBase64String(encryptedData2));
+                Console.WriteLine("\nDie Verschlüsselungen sind komplett verschieden, obwohl der Key gleich ist.");
+                Console.WriteLine("Das liegt zum Einen am Padding und zum Anderen an die Hashfunktionen, die verwendet werden.");
+                Console.WriteLine();
+
+                // 1b)
+                // Entschlüsseln Sie die beiden Ciphertexte wieder und prüfen Sie, ob Sie die ursprüngliche Nachricht erhalten.
+                decryptedData = RSADecrypt(encryptedData, privateKey, useOAEP);
+                decryptedData2 = RSADecrypt(encryptedData2, privateKey, useOAEP);
+                Console.WriteLine("Decrypted 1: {0}", ByteConverter.GetString(decryptedData));
+                Console.WriteLine("Decrypted 2: {0}", ByteConverter.GetString(decryptedData2));
+                Console.WriteLine("Es kommt die ursprüngliche Nachricht raus: {0}", ByteConverter.GetString(decryptedData) == ByteConverter.GetString(decryptedData2));
+
+                //1c)
+                //  Modifizieren Sie einen der Ciphertexte und entschlüsseln Sie ihn.
+                Console.WriteLine("Das erste Byte wird im Ciphertext verändert... ");
+                encryptedData2[0] = encryptedData2[0] == 0 ? (byte)1 : (byte)0;
+
+                Console.WriteLine("Nun wird die modifizierte Nachricht entschlüsselt...");
+                byte[] decryptedModfiedData = RSADecrypt(encryptedData2, privateKey, useOAEP);
+                Console.WriteLine("Decrypted Modified: {0}", ByteConverter.GetString(decryptedModfiedData));
+                
+                // Es wird eine Exception geworfen. Dazu gibt es eine Erklärung. Beim OAEP wird ein Padding
+                // erzeugt und durch diesen hat man einen Integritätsschutz. Es werden Nullen rangehängt. Und
+                // wenn beim entschlüsseln, die Nullen an der Nachricht nicht mehr existieren oder Anzahl ander ist,
+                // wird ein Fehler geworfen. 
+
+
+            }
+        }
+        catch (ArgumentNullException)
+        {
+            //Catch this exception in case the encryption did
+            //not succeed.
+            Console.WriteLine("Decrypting modified Data failed.");
+        }
+    }
+
+    public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+    {
+        try
+        {
+            byte[] encryptedData;
+            //Create a new instance of RSACryptoServiceProvider.
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+
+                //Import the RSA Key information. This only needs
+                //toinclude the public key information.
+                RSA.ImportParameters(RSAKeyInfo);
+
+                //Encrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later.  
+                encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+            }
+            return encryptedData;
+        }
+        //Catch and display a CryptographicException  
+        //to the console.
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.Message);
+
+            return null;
+        }
+    }
+
+    public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+    {
+        try
+        {
+            byte[] decryptedData;
+            //Create a new instance of RSACryptoServiceProvider.
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                //Import the RSA Key information. This needs
+                //to include the private key information.
+                RSA.ImportParameters(RSAKeyInfo);
+
+                //Decrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later.  
+                decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+            }
+            return decryptedData;
+        }
+        //Catch and display a CryptographicException  
+        //to the console.
+        catch (CryptographicException e)
+        {
+            Console.WriteLine("\n" + e.ToString());
+
+            return null;
+        }
+    }
+}
---
+++ U6/1/readme.md
@@ -0,0 +1,38 @@
+Die Beantwortung der Fragen finden Sie im Code als WriteLines und Kommentaren.  
+  
+Der Output vom Code und Comments: 
+
+
+// 1a)  
+// Verschlüsseln Sie die gleiche Nachricht Ihrer Wahl bei gleichem Schlüssel zweimal mit RSA OAEP.  
+Verschlüsselung mit gleichem Key.  
+  
+1: PmwehjEsfKc7XsKY5WDWCakucztNPSkMLLSsakvPSkRw1f9WKJyFPX9QRZXvdpQLDsJ2b4HE34N+5Y7KRwAPqvQkLwLkdfUm67wh/cHe7HaI360rKixIs/tadzVggQUVvwfUi2PIijHCW/4Xq4ODG/CeAdDj7w3m4alc9NsimqM=  
+  
+2: gIpDKyZvaNMr2QQOBB0jDAoZ0zEtvccjS2HfNz5+fyndbvzRx9DpvBwwrB30tfWNRFSpTHxk0bUmqYWpl3OnEKDJnu9fYX3NZ/PA3/WYY5YFFqH846L+3UQEd095UQ+WkyrDLPKVlI1lodeFTOUayOM9rYlkNSjnFqlttN+fNJY=  
+
+Die Verschlüsselungen sind komplett verschieden, obwohl der Key gleich ist.  
+Das liegt zum Einen am Padding und zum Anderen an die Hashfunktionen, die verwendet werden.  
+
+// 1b)  
+// Entschlüsseln Sie die beiden Ciphertexte wieder und prüfen Sie, ob Sie die ursprüngliche Nachricht erhalten.  
+Decrypted 1: Data to Encrypt
+Decrypted 2: Data to Encrypt
+Es kommt die ursprüngliche Nachricht raus: True
+  
+//1c)  
+//  Modifizieren Sie einen der Ciphertexte und entschlüsseln Sie ihn.  
+  
+Das erste Byte wird im Ciphertext verändert...
+Nun wird die modifizierte Nachricht entschlüsselt...  
+  
+// Es wird eine Exception geworfen. Dazu gibt es eine Erklärung. Beim OAEP wird ein Padding
+// erzeugt und durch diesen hat man einen Integritätsschutz. Es werden Nullen rangehängt. Und
+// wenn beim entschlüsseln, die Nullen an der Nachricht nicht mehr existieren oder Anzahl ander ist,
+// wird ein Fehler geworfen. 
+
+System.Security.Cryptography.CryptographicException: Cryptography_OAEPDecoding
+   at Internal.NativeCrypto.CapiHelper.DecryptKey(SafeKeyHandle safeKeyHandle, Byte[] encryptedData, Int32 encryptedDataLength, Boolean fOAEP, Byte[]& decryptedData)
+   at System.Security.Cryptography.RSACryptoServiceProvider.Decrypt(Byte[] rgb, Boolean fOAEP)
+   at RSAOAEP.RSADecrypt(Byte[] DataToDecrypt, RSAParameters RSAKeyInfo, Boolean DoOAEPPadding) in D:\Git\Gitlab\Krypto\U6\1\Program.cs:line 120
+Decrypting modified Data failed.
